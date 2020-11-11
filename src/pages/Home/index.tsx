fd2a3aae5cadb3b3e06b31d1d911b14e43d2ca65
Felipe V <mrchampz@outlook.com>
2020-11-11 17:51:08 -0300

    Post card

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,17 @@
 import React from "react"
 import { Container } from "react-bootstrap";
 
+import PostCard from "../../components/PostCard";
+
 export default function Home() {
   return (
-    <Container style={{ backgroundColor: 'red' }}>
-      Vamos de H1
+    <Container>
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
     </Container>
   );
 }
