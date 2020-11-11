c16767ca2b4c3e21351917d4870f7323572c8c02
Felipe V <mrchampz@outlook.com>
2020-11-11 17:58:53 -0300

    Fixed container size

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,21 @@
 import React from "react"
 import { Container } from "react-bootstrap";
 
+import { Styles } from './styles';
+
 import PostCard from "../../components/PostCard";
 
 export default function Home() {
   return (
-    <Container>
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-    </Container>
+    <Styles>
+      <Container>
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+        <PostCard />
+      </Container>
+    </Styles>
   );
 }
