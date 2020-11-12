6d14c7b53bda0178e4e5a3aebe2833b1ba0a14cf
Felipe V <mrchampz@outlook.com>
2020-11-12 18:01:59 -0300

    On scroll event

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useMedia } from 'react-media';
 import { Navbar, Form } from 'react-bootstrap';
 import { MediaQueries } from '../../utils/config';
@@ -16,6 +16,12 @@ export default function NavBar() {
   const isSmallScreen = useMedia({ query: MediaQueries.medium });
 
   const [showLogo, setShowLogo] = useState(true);
+  const [showBtnDownload, setShowBtnDownload] = useState(false);
+  const [showNavBarShadow, setShowNavBarShadow] = useState(false);
+
+  useEffect(() => {
+    window.onscroll = onPageScroll;
+  }, []);
 
   const onSearchFieldFocus = () => {
     if (isSmallScreen) {
@@ -26,6 +32,16 @@ export default function NavBar() {
   const onSearchFieldBlur = () => {
     if (!showLogo) {
       setShowLogo(true);
+    }
+  }
+
+  const onPageScroll = (event) => {
+    let offset = event.target.documentElement.scrollTop;
+    if (offset < 325 && showBtnDownload) {
+      setShowBtnDownload(false);
+    }
+    if (offset >= 325 && !showBtnDownload) {
+      setShowBtnDownload(true);
     }
   }
 
@@ -46,7 +62,12 @@ export default function NavBar() {
             <SearchIcon size={ 24 } />
           </SearchButton>
         </Form>
-        <DownloadButton className="ml-auto">Baixar Cartilha</DownloadButton>
+          <DownloadButton
+            visible={ showBtnDownload ? 'true' : 'false' }
+            className="ml-auto"
+          >
+            Baixar Cartilha
+          </DownloadButton>
       </Navbar>
     </Styles>
   );
--- src/styles/Home/styles.ts
+++ src/styles/Home/styles.ts
@@ -1,16 +1,5 @@
 import styled from 'styled-components';
-import media from 'styled-media-query';
 
 export const Styles = styled.div`
-  .container {
-    max-width: 70%;
-
-    ${ media.lessThan('medium')`
-      max-width: 90%;
-    `}
-
-    ${ media.lessThan('small')`
-      max-width: 100%;
-    `}
-  }
+  
 `;
