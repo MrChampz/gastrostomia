cfa83bf6dadbfc1f503f8ee9ce150b24c5d2b3da
Felipe V <mrchampz@outlook.com>
2020-11-20 18:10:16 -0300

    Showing images and fixed styling

--- src/styles/Page/styles.ts
+++ src/styles/Page/styles.ts
@@ -33,13 +33,50 @@ export const Pages = styled.div`
 export const Content = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
   justify-content: center;
+
+  h1, h2, h3, h4 {
+    margin-top: 15px;
+    margin-bottom: 30px;
+  }
+
+  h5, h6 {
+    margin-top: 10px;
+    margin-bottom: 20px;
+  }
 
   p {
     font-family: 'Roboto';
     font-size: 22px;
-    color: #6B6B6B;
+    color: #262626;
+  }
+
+  blockquote {
+    margin-top: 15px;
+    margin-bottom: 40px;
+    padding-top: 5px;
+    padding-bottom: 5px;
+    padding-left: 20px;
+    border-left: 5px solid #262626;
+    font-style: italic;
+    line-height: 1.75rem;
+
+    p {
+      margin: 0;
+      color: #6B6B6B;
+    }
+  }
+
+  ul, ol {
+    margin-top: 5px;
+    margin-bottom: 25px;
+  }
+
+  li {
+    margin: 5px 0;
+    font-family: 'Roboto';
+    font-size: 18px;
+    color: #262626;
   }
 `;
 
